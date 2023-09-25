cd17a049242345fee8d1428fa1fc25654f33e776
Michael Fotiadis <mikemike19991@gmail.com>
2023-09-25 21:51:04 +0300

    Player class.

---
+++ src/app/players-table/player.ts
@@ -0,0 +1,12 @@
+export class player {
+  constructor() {
+    index_id: Number;
+    id: Number;
+    name: String;
+    faction_name: String;
+    faction_position: String;
+    revivable: Boolean;
+    status: String;
+    time_stamp: Date;
+  }
+}
